f4c371e9e410952f9cafb8abd31035dbc2aab08a
idesovmarch <30424247+idesovmarch@users.noreply.github.com>
2018-08-01 09:17:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>hello frnd</title>
+
+  <!--Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- additional CSS -->
+  <link rel="stylesheet" href="css/styles.css">
+
+
+
+  <!--GoogleFonts-->
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700|Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Black+Han+Sans" rel="stylesheet">
+
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">brdhmmr</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php" style="font-family: 'Roboto', sans-serif;">Home</a></li>
+      <li><a href="portfolio.php" style="font-family: 'Roboto', sans-serif;">Zine</a></li>
+      <li><a href="contact.php" style="font-family: 'Roboto', sans-serif;">Contact</a></li>
+</ul>
+    <ul class="nav navbar-nav ml-auto" style="padding-left: 850px; position: auto;">
+          <li><a href="http://idesovmarch.tumblr.com"><span class="fab fa-tumblr-square fa-lg" style="color:Tomato"></span></a><li>
+          <li><a href="http://www.instagram.com/idesovmarch"><span class="fab fa-instagram fa-lg" style="color:Tomato"></span></a></li>
+          <li><a href="http://www.behance.net/idesovmarch"><span class="fab fa-behance fa-lg" style="color:Tomato"></span></a></li>
+        </ul>
+      </div>
+  </div>
+</nav>
+
+  </header>
+
+    <div class="container-fluid text-center" style="backgound-color: tomato;">
+
+      <img src="http://www.ohwhataworld.net/assets/photos/me1.jpg" class="img-circle" alt="Brad-Himself">
+      <div class="container-fluid">
+      <h1 class="page-header" style="font-family: 'Black Han Sans', sans-serif;">
+        Hey, I'm Brad.<br>
+      </h1>
+
+
+
+    <div id="rowx4" class="row text-center" style="background-color: tomato; margin-top: px;">
+      <div class="col-sm-4 col-md-6 col-lg-12" style="font-family: 'Black Han Sans', sans-serif; color: white;">
+        <h1>
+          I'm a Designer
+        </h1>
+      </div>
+</div>
+
+<div id="rowx4" class="row text-center">
+  <div class="col-sm-4 col-md-6 col-lg-12">
+    <p>
+      Right now this is a test website.
+    </p>
+  </div>
+</div>
+
+</div>
+  </div>
+
+  <footer class="container text-center">
+
+  </footer>
+
+  <!--Javascript-->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://use.fontawesome.com/releases/v5.0.9/js/all.js"></script>
+</body>
+
+</html>
